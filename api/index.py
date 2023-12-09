ce49892ad634e185a0b2071124420e4cf3365fef
loka shafeek <34503724+loka1@users.noreply.github.com>
2023-12-09 16:45:19 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirect_to_new_domain(path):
+    """
+    Redirects incoming requests to a new domain.
+
+    Args:
+        path (str): The path from the original request.
+
+    Returns:
+        Response: Redirects to the new domain with the provided path, if any.
+    """
     new_domain = 'http://phplin.com'
     new_url = f'{new_domain}/{path}' if path else new_domain
     return redirect(new_url, code=301)
